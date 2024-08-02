8c38bc4db47e1482b0f5d36d49b972fb2a814f2e
gitshubhsss <shubhamranjane16@gmail.com>
2024-08-02 22:22:53 +0530

    .\sb.java

---
+++ stringBuilder/sb.java
@@ -0,0 +1,13 @@
+public class sb {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("");
+       
+        for(char ch='a';ch<='z';ch++){
+            sb=sb.append(ch);
+           
+        }
+      
+
+
+    }
+}
